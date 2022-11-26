c07884c3309c0989095d646ee7d14e215c799284
Karsenh <KarsenHansen@gmail.com>
2022-11-26 00:05:34 -0800

    added pytesseract for image ocr (run energy dection)

---
+++ Scripts/Skilling/Agility/Gnome_Course.py
@@ -0,0 +1,21 @@
+
+
+def run_course():
+    # Define two sleep time lists; run_sleep_seconds & walk_sleep_seconds
+
+    # Check if starting in front of log or pipe - click corresponding start log xy
+
+    # Check run percentage and sleep for run or walk time
+
+    # Check break schedule
+
+    # Click next obstacle
+
+    # ---
+    # Create lists for (obstacle_xys, sleep_times (walk & run))
+    # click next obstacle xy (first obstacle has two potentials)
+    # check break schedule / break if needed
+    # check run energy % to decide which sleep_time list to choose from
+    # sleep for selected sleep time
+
+    return
