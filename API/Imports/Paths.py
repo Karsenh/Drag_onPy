c515c0ca50a3df79e4dba50b2aa2963d9414df40
Karsenh <KarsenHansen@gmail.com>
2022-12-16 23:41:38 -0800

    removed screenshots in root - added to assets dir

--- API/Imports/Paths.py
+++ API/Imports/Paths.py
@@ -3,7 +3,7 @@ import os
 
 proj_root_path = os.getcwd()
 
-ROOT_SCREENSHOTS_PATH = f'{proj_root_path}\Screenshots'
+ROOT_SCREENSHOTS_PATH = f'{proj_root_path}\Assets\Screenshots'
 ROOT_COMPARATORS_PATH = f'{ROOT_SCREENSHOTS_PATH}\Comparators'
 
 AUTH_SCREEN_PATH = f'{ROOT_COMPARATORS_PATH}\Auth'
@@ -11,7 +11,7 @@ BANKING_SCREEN_PATH = f'{ROOT_COMPARATORS_PATH}\Banking'
 GENERAL_SCREEN_PATH = f'{ROOT_COMPARATORS_PATH}\General'
 SCRIPTS_SCREEN_PATH = f'{ROOT_COMPARATORS_PATH}\Scripts'
 
-BS_SCREEN_PATH = f'{proj_root_path}\Screenshots\BlueStacks_ScreenShot.png'
+BS_SCREEN_PATH = f'{proj_root_path}\Assets\Screenshots\BlueStacks_ScreenShot.png'
 CUSTOM_IMG_PATH = f'{proj_root_path}\{ROOT_SCREENSHOTS_PATH}\Comparators\Custom'
 
 
