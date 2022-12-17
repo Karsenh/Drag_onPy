40a2c28690e4e6adbf8fc0945e38af22faf2658b
Karsenh <KarsenHansen@gmail.com>
2022-12-17 12:56:20 -0800

    tuned dart fletcher

--- Scripts/Skilling/Fletching/GE_Dart_Fletcher.py
+++ Scripts/Skilling/Fletching/GE_Dart_Fletcher.py
@@ -3,31 +3,30 @@ import API.AntiBan
 from API.Imaging.Image import wait_for_img
 from API.Mouse import mouse_click
 from API.Imaging.Image import does_img_exist
-from API.Interface.General import get_xy_for_invent_slot
+from API.Interface.General import get_xy_for_invent_slot, setup_interface, is_otd_enabled, is_tab_open
 from API.Debug import write_debug, log_to_debug
 
 
 def start_fletching_darts(curr_loop):
 
-    first_slot_xy = get_xy_for_invent_slot(1)
-    second_slot_xy = get_xy_for_invent_slot(2)
+    if not curr_loop == 1:
+        first_slot_xy = get_xy_for_invent_slot(1)
+        second_slot_xy = get_xy_for_invent_slot(2)
 
-    # if not does_img_exist(img_name="feathers", script_name="GE_Dart_Fletcher", threshold=0.95) \
-    #         or (not does_img_exist(img_name="bronze_dart_tip", script_name="GE_Dart_Fletcher", threshold=0.95) and not does_img_exist(img_name="steel_dart_tip", script_name="GE_Dart_Fletcher", threshold=0.90)):
-    #     msg = "Missing either feathers or dart tips - Exiting..."
-    #     write_debug(msg)
-    #     log_to_debug(msg)
-    #     return False
-
-    mouse_click(first_slot_xy)
-    mouse_click(second_slot_xy)
-
-    if not wait_for_img(img_name="Fletching_exp", script_name="GE_Dart_Fletcher", max_wait_sec=5, threshold=0.8):
-        return False
-
-    for i in range(1, random.randint(13, 27)):
         mouse_click(first_slot_xy)
         mouse_click(second_slot_xy)
-        # does_img_exist(img_name="feathers", script_name="GE_Dart_Fletcher", threshold=0.95, should_click=True)
-        # does_img_exist(img_name="bronze_dart_tip", script_name="GE_Dart_Fletcher", threshold=0.95, should_click=True)
+
+        if not wait_for_img(img_name="Fletching_exp", script_name="GE_Dart_Fletcher", max_wait_sec=5, threshold=0.8):
+            return False
+
+        for i in range(1, random.randint(13, 27)):
+            mouse_click(first_slot_xy)
+            mouse_click(second_slot_xy)
+    else:
+        setup_interface("north", 4, "down")
+        API.AntiBan.sleep_between(0.6, 0.9)
+        is_otd_enabled(False)
+        API.AntiBan.sleep_between(0.6, 0.9)
+        is_tab_open("inventory", should_open=True)
+
     return True
