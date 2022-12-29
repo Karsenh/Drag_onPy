55428a87747d84ed984749dee0c561b44aaf0308
Karsenh <KarsenHansen@gmail.com>
2022-12-29 02:17:09 -0800

    cleaned up UI to look respectable moving forward

--- GUI/Imports/Skill_Level_Input/Skill_Level_Input.py
+++ GUI/Imports/Skill_Level_Input/Skill_Level_Input.py
@@ -7,55 +7,58 @@ from osrs_api import Hiscores
 initial_get = True
 
 
-def show_skill_input_frame(skill_level_input_frame, t_active_frame, all_frames, username=""):
+def show_skill_input_frame(skill_level_input_frame, t_active_frame, all_frames, username_arg=""):
+    global initial_get
+
     break_font = tkinter.font.Font(family='Helvetica', size=11, weight='normal')
     break_btn_font = tkinter.font.Font(family='Helvetica', size=11, weight='bold')
-    global initial_get
 
     is_active = t_active_frame("skill", all_frames)
 
-    print(f'Show_Skill_Input_Frame Fired ')
-
-    assets_path = f"{os.getcwd()}\Assets"
-
-    search_username = ""
-
-    if username == "":
-        with open(f"{assets_path}\Levels.txt") as file:
-            search_username = file.readline()
-            search_username = search_username.replace("\n", "")
-
     skill_level_input_frame.grid(row=3, column=1, columnspan=5, pady=50)
 
-    username_var = tkinter.StringVar(skill_level_input_frame)
+    user_search_sub_frame = LabelFrame(skill_level_input_frame, text="Lookup Level by User", bg=label_frame_bg_color, font=break_font)
+    user_search_sub_frame.grid(row=1, pady=(20, 5))
+
+    print(f'Show_Skill_Input_Frame Fired')
+    assets_path = f"{os.getcwd()}\Assets"
+
+    username_from_file = ""
+
+    if username_arg == "":
+        with open(f"{assets_path}\Levels.txt") as file:
+            username_from_file = file.readline()
+            username_from_file = username_from_file.replace("\n", "")
+
+    username_var = tkinter.StringVar(user_search_sub_frame)
+
     if initial_get:
-        username_var.set(search_username)
+        username_var.set(username_from_file)
     else:
-        username_var.set(username)
+        username_var.set(username_arg)
 
-    username_label = Label(skill_level_input_frame, text="Search Username", background=frame_bg_color, font=break_font)
-    username_input = Entry(skill_level_input_frame, textvariable=username_var, background=label_frame_bg_color, font=break_btn_font)
+    # username_label = Label(user_search_sub_frame, text="Search Username", background=frame_bg_color, font=break_font)
+    username_input = Entry(user_search_sub_frame, textvariable=username_var, background=label_frame_bg_color, font=break_btn_font)
 
-    if search_username and initial_get:
-        print(f'Found username ({search_username}) in {assets_path}\Levels.txt')
-        user_hiscores = get_hiscores_for_user(search_username, skill_level_input_frame, t_active_frame, all_frames)
+    if username_from_file and initial_get:
+        print(f'Found username ({username_from_file}) in {assets_path}\Levels.txt')
+        user_hiscores = get_hiscores_for_user(username_from_file, user_search_sub_frame, t_active_frame, all_frames)
         initial_get = False
     else:
-        user_hiscores = get_hiscores_for_user(username, skill_level_input_frame, t_active_frame, all_frames)
+        print(f'Fetching HiScores for user_arg: {username_arg}')
+        user_hiscores = get_hiscores_for_user(username_arg, user_search_sub_frame, t_active_frame, all_frames)
 
-    username_label.grid(row=1, column=1)
-    username_input.grid(row=1, column=2, padx=20, pady=10)
+    # username_label.grid(row=1, column=1)
+    username_input.grid(row=1, column=1, padx=20, pady=10)
 
-    username_lookup_btn = Button(skill_level_input_frame, text="Search User", bg=btn_bg_color, activebackground=btn_active_bg_color, command=lambda: show_skill_input_frame(skill_level_input_frame, t_active_frame, all_frames, username=username_input.get()))
+    username_lookup_btn = Button(user_search_sub_frame, font=break_btn_font, text="Search User", bg=btn_bg_color, activebackground=btn_active_bg_color, command=lambda: show_skill_input_frame(skill_level_input_frame, t_active_frame, all_frames, username_arg=username_input.get()))
 
     username_lookup_btn.grid(row=2, column=1, columnspan=3, pady=10)
 
-
-    skill_level_input_sub_frame = LabelFrame(skill_level_input_frame, text="Levels", bg=label_frame_bg_color, font=break_font)
-
-    skill_level_input_sub_frame.grid(row=4)
-
     # SKILL LEVEL INPUTS
+    skill_level_input_sub_frame = LabelFrame(skill_level_input_frame, text="Levels", bg=label_frame_bg_color, font=break_font, padx=50, pady=20)
+    skill_level_input_sub_frame.grid(row=2)
+
     attack_level_var = tkinter.StringVar(skill_level_input_frame)
     attack_level_var.set(user_hiscores.skills['attack'].level)
     attack_label = Label(skill_level_input_sub_frame, text="Attack:", background=frame_bg_color, font=break_font)
@@ -76,9 +79,6 @@ def show_skill_input_frame(skill_level_input_frame, t_active_frame, all_frames, 
     mining_input = Entry(skill_level_input_sub_frame, textvariable=mining_level_var, background=label_frame_bg_color, font=break_btn_font, width=3)
     mining_label.grid(row=3, column=5)
     mining_input.grid(row=3, column=6)
-
-
-    # Break Timer Frame
 
     return
 
