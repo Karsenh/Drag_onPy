be7c1233a87580081a3b7449a33f6e33e99d09e0
Karsenh <KarsenHansen@gmail.com>
2022-12-31 17:57:30 -0800

    moved camera level out and reshot ge_bank screenshot

--- Test.py
+++ Test.py
@@ -15,7 +15,9 @@ clear_debug_log()
 # show_main_gui()
 launch_script("GE_Dhide_Bodies")
 
-# wait_for_img(img_name="Ge_bank", script_name="GE_Dhide_Bodies", threshold=0.99, should_click=True, x_offset=30)
+
+# setup_interface("north", 4, "up")
+# wait_for_img(img_name="Ge_bank", script_name="GE_Dhide_Bodies", threshold=0.85, should_click=True, x_offset=30)
 # wait_for_img(img_name="Banked_green_leather", script_name="GE_Dhide_Bodies", should_click=True, x_offset=35, img_sel="first")
 
 
