0f32f27f807b9bfe6c589764b3054654181f1ab3
Karsenh <KarsenHansen@gmail.com>
2023-01-01 17:10:48 -0800

    Rough script complete

--- Scripts/Skilling/Woodcutting/Cwars_Teaks.py
+++ Scripts/Skilling/Woodcutting/Cwars_Teaks.py
@@ -3,7 +3,7 @@ import random
 import API.AntiBan
 from API.Mouse import mouse_click, mouse_long_click
 from API.Interface.General import setup_interface, is_tab_open, get_xy_for_invent_slot
-from API.Interface.Bank import is_bank_open, is_bank_tab_open, close_bank, deposit_all
+from API.Interface.Bank import is_bank_open, is_bank_tab_open, close_bank, deposit_all, is_withdraw_qty
 from API.Imaging.Image import does_img_exist, wait_for_img, get_existing_img_xy
 
 script_name = "Cwars_Teak"
@@ -15,8 +15,8 @@ axe_type = "dragon"
 axe_equipped = False
 rod_equipped = False
 
+should_bank = True
 should_use_rod = True
-should_bank = False
 
 teak_attempts = 0
 clicked_teak = False
@@ -44,9 +44,21 @@ def start_chopping_teaks(curr_loop):
             if should_bank:
                 if should_use_rod:
                     print(f'Using rod to cwars bank')
-                    teleport_to_cwars()
+                    teleport_to_cwars_bank()
                 else:
                     print(f'Running back to cwars bank')
+                    run_to_cwars_bank()
+
+                move_to_chest()
+                if not open_cwars_bank():
+                    move_to_chest()
+                    if not open_cwars_bank():
+                        print(f'Failed to open cwars bank a couple times. Exiting')
+                        return False
+                deposit_teaks()
+                inventory_full = False
+                move_to_teak_tree()
+                clicked_teak = chop_teak()
             else:
                 print(f'Dropping logs')
                 while does_img_exist(img_name="Inventory_teak", script_name=script_name, should_click=True):
@@ -55,20 +67,27 @@ def start_chopping_teaks(curr_loop):
         # Check if we're still chopping teak - check if it's because inventory full or if we just need to reclick
         if is_still_chopping():
             clicked_teak = False
+            print(f'SAW EXP - STILL CHOPPING: teak_attemps = {teak_attempts} (now 0) & clicked_teak = {clicked_teak}')
             teak_attempts = 0
             return True
         else:
             # If we clicked the teak but we're not still chopping
             if clicked_teak:
-                if teak_attempts > 1:
-                    inventory_full = True
                 teak_attempts += 1
+                print(f"Clicked_teak previously but didn't see exp - increasing teak_attempts = {teak_attempts}")
+                if teak_attempts > 1:
+                    print(f'Exceeded teak_attempts = {teak_attempts}\nInventory_full = {inventory_full} RETURNING.')
+                    inventory_full = True
+                    return True
 
             clicked_teak = chop_teak()
+            print(f'🪓 Clicked to chop teak - clicked_teak = {clicked_teak}')
+            return True
 
     else:
         print(f'This is the first loop')
         setup_interface("west", 2, "up")
+
         # (Start at the cwars bank)
         # Open equipment tab to check if we have selected axe equipped
         axe_equipped = is_axe_equipped()
@@ -85,7 +104,8 @@ def start_chopping_teaks(curr_loop):
 
         # If no axe equipped - go to wc'ing tab and withdraw - equip
         if not axe_equipped:
-            withdraw_axe()
+            if withdraw_axe():
+                deposit_all()
 
         # If should_use_rod and need_new_rod, go to jewelry tab and withdraw - equip
         if should_bank:
@@ -116,11 +136,13 @@ def is_axe_equipped():
     # Open equipment tab
     is_tab_open("equipment", True)
     # Check for ring of dueling
-    return does_img_exist(img_name=f"Equipped_{axe_type}_axe", script_name=script_name)
+    return wait_for_img(img_name=f"Equipped_{axe_type}_axe", script_name=script_name, max_wait_sec=3)
 
 
 def open_cwars_bank():
-    does_img_exist(img_name="Cwars_bank", script_name=script_name, should_click=True)
+    if wait_for_img(img_name="Cwars_bank", script_name=script_name):
+        API.AntiBan.sleep_between(0.7, 0.8)
+        does_img_exist(img_name="Cwars_bank", script_name=script_name, should_click=True)
     return is_bank_open()
 
 
@@ -134,36 +156,50 @@ def withdraw_axe():
 
 def withdraw_new_rod():
     is_bank_tab_open(tab_num=jewelry_tab_num, should_open=True)
+    is_withdraw_qty(qty="1", should_click=True)
     does_img_exist(img_name=f"Banked_rod", script_name=script_name, should_click=True, x_offset=15, y_offset=5)
-    API.AntiBan.sleep_between(0.4, 0.6)
+    API.AntiBan.sleep_between(0.9, 1.1)
     mouse_long_click(get_xy_for_invent_slot(1))
     return wait_for_img(img_name="Wear_rod", script_name=script_name, should_click=True)
 
 
 def deposit_teaks():
-    # does_img_exist(img_name="Inventory_teak", script_name=script_name, should_click=True)
+    is_withdraw_qty(qty="all", should_click=True)
     invent_teak_xy = get_xy_for_invent_slot(slot_num=random.randint(9, 20))
     mouse_click(invent_teak_xy)
     return
 
 
 def move_to_teak_tree():
-    for i in range(0, 8):
-        if not wait_for_img(img_name=f"teak_path_{i}", script_name=script_name, should_click=True, threshold=0.90, max_wait_sec=15, y_offset=6):
-            print(f"Couldn't find teak_path_{i} - Exiting.")
-            return False
+    for i in range(0, 9):
+        if i < 8:
+            if wait_for_img(img_name=f"teak_path_{i}", script_name=script_name, threshold=0.93, max_wait_sec=20):
+                if not does_img_exist(img_name=f"teak_path_{i}", script_name=script_name, threshold=0.93, max_wait_sec=20, should_click=True):
+                    print(f'does_path_img secondary search failed {i}.')
+                    return False
+            else:
+                print(f"Primary path_img search failed {i}")
+                return False
+        else:
+            if wait_for_img(img_name="teak_path_8", script_name="Cwars_Teak", threshold=0.90, x_offset=-4, y_offset=6):
+                if not does_img_exist(img_name="teak_path_8", script_name="Cwars_Teak", threshold=0.90, x_offset=-4, y_offset=6, should_click=True):
+                    print(f'Secondary last path search failed.')
+                    return False
+            else:
+                print(f'Primary path search ({i}) not found')
+                return False
     return True
 
 
 def chop_teak():
-    return wait_for_img(img_name="Teak_tree", script_name=script_name, should_click=True, max_wait_sec=10)
+    return wait_for_img(img_name="Teak_tree", script_name=script_name, should_click=True, max_wait_sec=10, threshold=0.8)
 
 
 def run_to_cwars_bank():
     return
 
 
-def teleport_to_cwars():
+def teleport_to_cwars_bank():
     is_tab_open("equipment", True)
     does_img_exist(img_name="Equipped_rod", script_name=script_name)
     mouse_long_click(get_existing_img_xy())
@@ -175,11 +211,11 @@ def drop_teaks():
 
 
 def is_still_chopping():
-    return wait_for_img(img_name="Woodcutting", category="Exp_Drops", max_wait_sec=10)
+    return wait_for_img(img_name="Woodcutting", category="Exp_Drops", max_wait_sec=8)
 
 
 def move_to_chest():
-    return does_img_exist(img_name="bank_minimap", script_name=script_name, should_click=True)
+    return wait_for_img(img_name="bank_minimap", script_name='Cwars_Teak', should_click=True, x_offset=6)
 
 # -------
 # HELPERS
--- Test.py
+++ Test.py
@@ -4,6 +4,8 @@ from GUI.Main_GUI import *
 from API.Debug import clear_debug_log
 from API.Imaging.Image import *
 from API.Imaging.OCR.Run_Energy import *
+from API.Imports.Paths import BS_SCREEN_PATH
+from Scripts.Skilling.Woodcutting.Cwars_Teaks import is_rod_equipped, withdraw_new_rod
 
 
 get_bluestacks_xy()
@@ -15,9 +17,21 @@ clear_debug_log()
 # show_main_gui()
 launch_script("Cwars_Teak")
 
-# #
-# for i in range(6, 8):
-#     if not wait_for_img(img_name=f"teak_path_{i}", script_name="Cwars_Teak", should_click=True, threshold=0.95, max_wait_sec=15, y_offset=2, x_offset=2):
+
+# does_img_exist(img_name="teak_path_8", script_name="Cwars_Teak", threshold=0.90, should_click=True, x_offset=-4)
+
+# wait_for_img(img_name="bank_minimap", script_name='Cwars_Teak', should_click=True, x_offset=6)
+
+# does_img_exist(img_name="Teak_tree", script_name="Cwars_teak", should_click=True, threshold=0.8)
+# open_color = 106, 35, 26
+# find_color_xy(BS_SCREEN_PATH, open_color)
+
+# does_img_exist(img_name="Equipped_rod", script_name="Cwars_Teak")
+# wait_for_img(img_name="Teak_tree", script_name="Cwars_Teak", should_click=True, max_wait_sec=10)
+
+# for i in range(5, 9):
+#     if not wait_for_img(img_name=f"teak_path_{i}", script_name="Cwars_Teak", should_click=True, threshold=0.85,
+#                         max_wait_sec=20):
 #         print(f"Couldn't find teak_path_{i} - Exiting.")
 
 # does_img_exist(img_name="Teak_path_7", script_name="Cwars_Teak", threshold=0.9, should_click=True)
