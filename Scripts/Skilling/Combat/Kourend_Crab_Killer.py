2cc3dbbe021274e6ebb83b5cf75cdbe87f8fd85f
Karsenh <KarsenHansen@gmail.com>
2023-01-14 01:41:15 -0800

    reworked crab killer for a single spot support at the moment

--- Scripts/Skilling/Combat/Kourend_Crab_Killer.py
+++ Scripts/Skilling/Combat/Kourend_Crab_Killer.py
@@ -2,52 +2,69 @@ import API.AntiBan
 from API.Interface.General import setup_interface, is_tab_open, is_run_on, is_run_gt
 from API.Imaging.Image import wait_for_img, does_img_exist
 from API.Mouse import mouse_click
+from API.Debug import write_debug
 
 SCRIPT_NAME = "Kourend_Crab_Killer"
 
 # Attack, Strength, Defense
 ATTACK_STYLE = "Defense"
 # 1 = NW tip (4 crabs)
-CRAB_SPOT = 1
-
 CURR_SPOT = 1
 
 
 def start_killing_kourend_crabs(curr_loop):
     global ATTACK_STYLE
+    global CURR_SPOT
+
     if curr_loop != 1:
         if not is_fighting_crab(wait=8):
             print(f'Not fighting crabs - Resetting aggro')
-            reset_aggro()
+            handle_run()
+            if not reset_spot_1():
+                return False
 
     else:
-        setup_interface("east", 1, "up")
+        setup_interface("west", 1, "up")
         set_attack_style(ATTACK_STYLE)
-        set_curr_spot()
+        # set_curr_spot()
     return True
 
 
-def reset_aggro():
-    global CURR_SPOT
+def reset_spot_1():
+    RESET_IMG_THRESH = 0.96
 
-    handle_run()
+    if not does_img_exist(img_name=f"Reset_Spot_1_Agg_A", script_name="Kourend_Crab_Killer",
+                          threshold=RESET_IMG_THRESH, should_click=True, x_offset=30, y_offset=28):
+        manual_xy = 1324, 296
+        mouse_click(manual_xy)
 
-    if CURR_SPOT == 1:
-        print(f'case 1')
-        return move_to_spot(2)
-    else:
-        print(f'case 2')
-        return move_to_spot(1)
+    # Arrow image before heading back
+    if not wait_for_img(img_name=f"Reset_Spot_1_Agg_B", script_name="Kourend_Crab_Killer",
+                        threshold=RESET_IMG_THRESH, should_click=True, max_wait_sec=10):
+        manual_xy = 1367, 270
+        mouse_click(manual_xy)
+
+    API.AntiBan.sleep_between(4.5, 4.6)
+
+    if not wait_for_img(img_name=f"Reset_Spot_1_Agg_Back_A", script_name="Kourend_Crab_Killer",
+                        threshold=RESET_IMG_THRESH, should_click=True, x_offset=40, max_wait_sec=10):
+        manual_xy = 1367, 270
+        mouse_click(manual_xy)
+
+    API.AntiBan.sleep_between(6.5, 6.6)
+
+    return wait_for_img(img_name="At_Spot_1", script_name="Kourend_Crab_Killer",
+                 threshold=0.97, should_click=True, x_offset=55, y_offset=0, max_wait_sec=10)
 
 
 def set_curr_spot():
     global CURR_SPOT
 
     if does_img_exist(img_name="At_Spot_1", script_name=SCRIPT_NAME, threshold=0.9):
-        print(f'AT SPOT ONE')
+        print(f'✅ AT SPOT ONE')
         CURR_SPOT = 1
     elif does_img_exist(img_name="At_Spot_2", script_name=SCRIPT_NAME, threshold=0.9):
-        print(f'AT SPOT TWO')
+        print(f'✅ AT SPOT TWO')
         CURR_SPOT = 2
     else:
         print(f'Couldnt find either spot images - set to none!')
@@ -74,6 +91,9 @@ def set_attack_style(style):
     return False
 
 
+# -------
+# HELPERS
+# -------
 def handle_run():
     if is_run_gt(percent=9):
         if is_run_on():
@@ -102,38 +122,3 @@ def handle_run():
                 else:
                     # API.AntiBan.sleep_between(28.0, 28.1)
                     return 28
-
-
-# -------
-# HELPERS
-# -------
-
-def move_to_spot(spot_num):
-    global CURR_SPOT
-
-    if does_img_exist(img_name=f"Move_To_Spot_{spot_num}_A", script_name="Kourend_Crab_Killer", threshold=0.9,
-                      should_click=True, click_middle=True):
-        API.AntiBan.sleep_between(4.0, 4.1)
-        if wait_for_img(img_name=f"Move_To_Spot_{spot_num}_B", script_name="Kourend_Crab_Killer", threshold=0.9,
-                          should_click=True, click_middle=True, max_wait_sec=10):
-            API.AntiBan.sleep_between(5.0, 5.1)
-            CURR_SPOT = spot_num
-            return True
-        else:
-            # Cant find second move to spot image moving back to original spots
-            does_img_exist(img_name=f"Cancel_Move_To_{spot_num}")
-
-    else:
-        # First move to spot img not found - we're likely not on right tile - recenter
-        if CURR_SPOT == 2:
-            if does_img_exist(img_name=f"Reposition_Spot_2", script_name=SCRIPT_NAME, should_click=True, threshold=0.9, x_offset=-35, y_offset=40):
-                return True
-            else:
-                return False
-        elif CURR_SPOT == 1:
-            if does_img_exist(img_name=f"Reposition_Spot_1", script_name=SCRIPT_NAME, threshold=0.9, should_click=True):
-                return True
-            else:
-                return False
-
-        return False
--- Test.py
+++ Test.py
@@ -1,3 +1,4 @@
+import API.AntiBan
 from GUI.Main_GUI import *
 from API.Debug import clear_debug_log
 from API.Imaging.Image import *
@@ -35,75 +36,11 @@ def __main__():
 
     # show_main_gui()
 
-    # launch_script("Kourend_Crab_Killer")
-
-    # does_img_exist(img_name=f"Reposition_Spot_2", script_name="Kourend_Crab_Killer", should_click=True, threshold=0.9,
-    #                x_offset=-40, y_offset=35)
-
-    does_img_exist(img_name='Move_To_Spot_2_A', script_name="Kourend_Crab_Killer", threshold=0.9, should_click=True, click_middle=True)
+    launch_script("Kourend_Crab_Killer")
 
 
 
-    # does_img_exist(img_name="Move_To_Spot_2_A", script_name="Kourend_Crab_Killer", threshold=0.9, should_click=True, click_middle=True)
 
-
-    # wait_for_img(img_name="Claim_Sack", script_name="Motherlode_Miner", threshold=0.90, should_click=True, click_middle=True)
-
-    # does_img_exist(img_name="Minimap_Midway_From_Box_Alt", script_name="Motherlode_Miner", threshold=0.95, should_click=True, x_offset=-1)
-    # does_img_exist(img_name="Minimap_Midway_From_Box", script_name="Motherlode_Miner", threshold=0.9, should_click=True, y_offset=4)
-    # is_tab_open("inventory", False)
-    # does_img_exist(img_name="Back_to_Spot_1", script_name="Motherlode_Miner", threshold=0.95, should_click=True, click_middle=True)
-
-    # MOTHERLODE MINER
-    # does_img_exist(img_name="Minimap_Midway", script_name="Motherlode_Miner", threshold=0.9, should_click=True)
-    # does_img_exist(img_name="Midway_Rock_From_Ore", script_name="Motherlode_Miner", threshold=0.9, should_click=True)
-
-    # Wait for Mining exp drop as confirmation obstacle was cleared
-    # wait_for_img(img_name="Mining", category="Exp_Drops", threshold=0.90, max_wait_sec=10)
-    # does_img_exist(img_name="Minimap_Sluice", script_name="Motherlode_Miner", threshold=0.9, should_click=True, click_middle=True)
-    # is_tab_open("inventory", False)
-    # wait_for_img(img_name="Running_Water", script_name="Motherlode_Miner", threshold=0.5, should_click=True, click_middle=True)
-
-    # does_img_exist(img_name="Broken_Wheel_2_From_1", script_name="Motherlode_Miner", threshold=0.98, should_click=True, click_middle=True)
-    # wait_for_img(img_name="Smithing", category="Exp_Drops", threshold=0.9)
-
-    # does_img_exist(img_name="Broken_Wheel_2", script_name="Motherlode_Miner", threshold=0.98, should_click=True, click_middle=True)
-    # wait_for_img(img_name="Smithing", category="Exp_Drops", threshold=0.9)
-    #
-    # does_img_exist(img_name="Broken_Wheel_2_From_1", script_name="Motherlode_Miner", threshold=0.98, should_click=True, click_middle=True)
-    # API.AntiBan.sleep_between(1.0, 1.1)
-    # wait_for_img(img_name="Smithing", category="Exp_Drops", threshold=0.9)
-    #
-    # wait_for_img(img_name="Hopper_From_Wheel_2", script_name="Motherlode_Miner", threshold=0.95, should_click=True, click_middle=True)
-    # wait_for_img(img_name="Claim_Sack", script_name="Motherlode_Miner", threshold=0.98, should_click=True, click_middle=True)
-    # wait_for_img(img_name="Claimed_Ore", script_name="Motherlode_Miner", threshold=0.98)
-    # minimap_deposit_box = 1368, 237
-    # mouse_click(minimap_deposit_box)
-    # wait_for_img(img_name="Deposit_Box", script_name="Motherlode_Miner", threshold=0.9, should_click=True, click_middle=True)
-    # wait_for_img(img_name="Inventory_Btn", script_name="Motherlode_Miner", threshold=0.9, should_click=True, click_middle=True)
-    # wait_for_img(img_name="Hammer_Box", script_name="Motherlode_Miner", threshold=0.9, should_click=True, click_middle=True)
-
-    # Something here to move to midway rock
-    # wait_for_img(img_name="Back_to_Midway", script_name="Motherlode_Miner", threshold=0.9, should_click=True)
-
-    # wait_for_img(img_name="Midway_Rock_From_Sluice", script_name="Motherlode_Miner", threshold=0.9, should_click=True, click_middle=True)
-    #
-    # back_to_spot_1 = 753, 206
-    # mouse_click(back_to_spot_1)
-    # spot = "Corner"
-    # does_img_exist(img_name=f"Hopper_From_{spot}", script_name="Motherlode_Miner", threshold=0.95, should_click=True,
-    #                click_middle=True)
-    # wait_for_img(img_name="Claim_Sack", script_name="Motherlode_Miner", threshold=0.98, should_click=True, click_middle=True)
-
-    # wait_for_img(img_name="Minimap_Midway_From_Box", script_name="Motherlode_Miner", threshold=0.95, should_click=True, y_offset=3)
-    # wait_for_img(img_name="Midway_Rock_From_Box", script_name="Motherlode_Miner", threshold=0.95, should_click=True, y_offset=3)
-
-
-    # CURR_SPOT = 9
-    #
-    # does_img_exist(img_name=f"Spot_{CURR_SPOT+1}_From_{CURR_SPOT}", script_name="Motherlode_Miner", threshold=0.98, should_click=True, click_middle=True)
-    #
-    # wait_for_img(img_name="Mining", category="Exp_Drops", threshold=0.92, max_wait_sec=10)
 
 
     return
