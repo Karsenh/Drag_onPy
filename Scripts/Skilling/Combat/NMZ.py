f44ed918622ffb8a7b7140ba001503aaff0887f1
Karsenh <KarsenHansen@gmail.com>
2023-02-28 09:29:25 -0800

    refined nmz script laptop

--- Scripts/Skilling/Combat/NMZ.py
+++ Scripts/Skilling/Combat/NMZ.py
@@ -124,7 +124,7 @@ def click_inventory_cake():
 
     print(f'Move to clicking instead of guzzling...')
 
-    while needs_rock_cake():
+    while needs_rock_cake() and not needs_ovl():
         mouse_click(get_cached_rock_cake_xy())
 
     return True
@@ -155,7 +155,7 @@ def restart_dream():
         return False
 
     # 7. Confirm we're in dream and return True if so - else return False
-    return is_in_dream()
+    return True
 
 
 # -------
@@ -337,7 +337,7 @@ def needs_abs():
     abs_region_check = 113, 250, 188, 290
     abs_color = (255, 129, 129)
 
-    return does_color_exist_in_sub_image(abs_region_check, abs_color, 'is_absorption_active', color_tolerance=30, count_min=50)
+    return does_color_exist_in_sub_image(abs_region_check, abs_color, 'is_absorption_active', color_tolerance=40, count_min=20)
 
 
 def needs_ovl():
