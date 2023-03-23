40c395c5bb6612ad40abfc031c6f8fe53b14e677
Karsenh <KarsenHansen@gmail.com>
2023-03-23 00:44:05 -0700

    added timer | added failsafe stop to finished pots

--- API/Time.py
+++ API/Time.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from API.Debug import write_debug
 
+START_TIME = None
+
 
 def is_time_up(start_time, max_downtime_sec):
     curr_time = datetime.now()
@@ -10,6 +12,21 @@ def is_time_up(start_time, max_downtime_sec):
     if time_diff.total_seconds() > max_downtime_sec:
         # write_debug(f'Time is up!')
         return True
-    else:
-        # write_debug(f'Time is not up yet...')
-        return False
+    # write_debug(f'Time is not up yet...')
+    return False
+
+
+def start_script_timer():
+    # Sets START_TIME global
+    global START_TIME
+    START_TIME = datetime.now()
+    return
+
+
+def get_curr_runtime():
+    # Returns the difference in time from now to the time we started
+    curr_time = datetime.now()
+
+    curr_runtime = curr_time - START_TIME
+    write_debug(f'CURRENT RUN-TIME: {curr_runtime}')
+    return
