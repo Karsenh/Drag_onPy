ca7c71ca7e2d16390899c8bf53ab5c1cda3915f8
Karsenh <KarsenHansen@gmail.com>
2023-05-13 02:37:33 -0700

    refined teleport using crafting cape with cached xy

--- API/Actions/Teleporting.py
+++ API/Actions/Teleporting.py
@@ -3,7 +3,10 @@ from API.Interface.General import is_tab_open
 from API.Mouse import mouse_long_click, mouse_click
 
 
+CACHED_INVENT_CRAFTING_CAPE_XY = None
+
 def teleport_with_crafting_cape(is_equipped=False):
+    global CACHED_INVENT_CRAFTING_CAPE_XY
     print(f'Teleporting with Crafting Cape...')
 
     # Open the tab which the crafting cape is in (equipment or inventory)
@@ -22,11 +25,16 @@ def teleport_with_crafting_cape(is_equipped=False):
         return False
 
     # Adjust the cape coords for clicking
-    cape_x, cape_y = get_existing_img_xy()
-    adj_xy = cape_x + 10, cape_y + 10
+    if CACHED_INVENT_CRAFTING_CAPE_XY:
+        mouse_long_click(CACHED_INVENT_CRAFTING_CAPE_XY)
+    else:
+        cape_x, cape_y = get_existing_img_xy()
+        adj_xy = cape_x + 10, cape_y + 10
 
-    # Long click for options
-    mouse_long_click(adj_xy)
+        CACHED_INVENT_CRAFTING_CAPE_XY = adj_xy
+
+        # Long click for options
+        mouse_long_click(adj_xy)
 
     # Select teleport option
     if not does_img_exist(img_name='crafting_cape_teleport_option', category='Teleports', should_click=True, click_middle=True):
