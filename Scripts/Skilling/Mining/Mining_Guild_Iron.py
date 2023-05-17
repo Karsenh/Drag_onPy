08f237e645f7658c5a44363c69b5e94edd7b60af
Karsenh <KarsenHansen@gmail.com>
2023-05-17 16:18:00 -0700

    refined mining guild script and added relog check

--- Scripts/Skilling/Mining/Mining_Guild_Iron.py
+++ Scripts/Skilling/Mining/Mining_Guild_Iron.py
@@ -4,8 +4,9 @@ import API.AntiBan
 from API.Imaging.Image import wait_for_img, does_img_exist, get_existing_img_xy, does_color_exist, \
     does_color_exist_in_thresh
 from API.Imports.Paths import BS_SCREEN_PATH
-from API.Interface.General import setup_interface, is_tab_open, is_otd_enabled, drop_inventory
+from API.Interface.General import setup_interface, is_tab_open, is_otd_enabled, drop_inventory, relog
 from API.Mouse import mouse_click, mouse_long_click
+from API.Time import get_curr_runtime, reset_curr_runtime
 
 SCRIPT_NAME = 'Mining_Guild_Iron'
 
@@ -30,6 +31,13 @@ def start_mining_guild_iron(curr_loop):
         while not inventory_full():
             mine_iron()
 
+        curr_rt = get_curr_runtime()
+
+        if curr_rt.total_seconds() > 19800:
+            relog()
+            setup_interface("south", 5, "up")
+            reset_curr_runtime()
+
         if not deposit_inventory():
             print(f'⛔ Deposite inventory failed for some reason - likely found iron in inventory after attempting to deposit - Exiting...')
             return False
@@ -39,6 +47,8 @@ def start_mining_guild_iron(curr_loop):
 
         if is_dpick_spec_ready():
             use_spec()
+
+
 
     else:
         print(f'First loop')
@@ -79,10 +89,11 @@ def deposit_inventory():
         return False
 
     keyboard.send('esc')
+    return True
 
-    API.AntiBan.sleep_between(1.0, 1.1)
-
-    return not does_img_exist(img_name='inventory_iron_ore', script_name=SCRIPT_NAME, img_sel='inventory')
+    # API.AntiBan.sleep_between(1.0, 1.1)
+    #
+    # return not does_img_exist(img_name='inventory_iron_ore', script_name=SCRIPT_NAME, img_sel='inventory')
 
 
 def move_back_to_ore():
