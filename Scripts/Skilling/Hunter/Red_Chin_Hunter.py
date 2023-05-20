fb18f69d6f7703be9feb6c4d8b595d2f679acdb1
Karsenh <KarsenHansen@gmail.com>
2023-05-20 00:40:43 -0700

    refined script a bit

--- Scripts/Skilling/Hunter/Red_Chin_Hunter.py
+++ Scripts/Skilling/Hunter/Red_Chin_Hunter.py
@@ -36,7 +36,7 @@ class SpotCoords:
 
 
 t1f1_ss_region_xy = 830, 425, 880, 520
-t2f1_ss_region_xy = 622, 425, 668, 510
+t2f1_ss_region_xy = 590, 422, 668, 510
 t3f1_ss_region_xy = 840, 663, 885, 745
 t4f1_ss_region_xy = 580, 657, 635, 730
 t5f1_ss_region_xy = 712, 541, 760, 625
